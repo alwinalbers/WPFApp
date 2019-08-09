8ac3c07916dd3e29cb4470eecf81c8d710942340
Albers Alwin <AlbersAl@gkmads.net>
2019-08-09 15:30:15 +0200

    Initial commit, base MVVM architecture implemented

---
+++ WPFApplication/ViewModel/PersonViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WPFApplication.ViewModel
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private Person _person;
+
+        public PersonViewModel()
+        {
+            _person = new Person { Birthday = new DateTime(1,1,1) ,  Name = "Max Mustermann" };
+        }
+
+        public DateTime PersonBirthday
+        {
+            get { return _person.Birthday; }
+            set
+            {
+                _person.Birthday = value;
+                OnPropertyChanged("PersonBirthday");
+            }
+        }
+
+        public string PersonName
+        {
+            get { return _person.Name; }
+
+            set
+            {
+                _person.Name = value;
+                OnPropertyChanged("PersonName");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                PropertyChanged(this, e);
+            }
+        }
+    }
+}
