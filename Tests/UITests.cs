3b44233f8def23705161e324d894fffd08e4339f
Alwin <alwinalbers@yyahoo.de>
2019-08-19 22:10:16 +0200

    added Test fundaments

---
+++ Tests/UITests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class UITests
+    {
+        private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        private const string AppId = @"D:\work\WPFApp\WPFApplication\bin\Debug\WPFApplication.exe";
+        protected static WindowsDriver<WindowsElement> session;
+
+        public static void Setup(TestContext context)
+        {
+            // Launch Calculator application if it is not yet launched
+            if (session == null)
+            {
+                // Create a new session to bring up an instance of the Calculator application
+                // Note: Multiple calculator windows (instances) share the same process Id
+                DesiredCapabilities appCapabilities = new DesiredCapabilities();
+                appCapabilities.SetCapability("app", AppId);
+                appCapabilities.SetCapability("deviceName", "WindowsPC");
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+                Assert.IsNotNull(session);
+
+                // Set implicit timeout to 1.5 seconds to make element search to retry every 500 ms for at most three times
+                session.Manage().Timeouts().ImplicitlyWait(new TimeSpan(2));
+            }
+        }
+
+        public static void TearDown()
+        {
+            // Close the application and delete the session
+            if (session != null)
+            {
+                session.Quit();
+                session = null;
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
