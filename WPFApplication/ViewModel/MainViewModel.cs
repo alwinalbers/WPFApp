cbd55844b5fa15871777f6f7c716ece111561caf
alwinalbers <alwinalbers@yahoo.de>
2019-08-22 10:44:54 +0200

    Removed unnecessary usings

--- WPFApplication/ViewModel/MainViewModel.cs
+++ WPFApplication/ViewModel/MainViewModel.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using WPFApplication.Models;
-using System.Windows;
 using System.Windows.Input;
+using WPFApplication.Models;
 
 namespace WPFApplication.ViewModel
 {
